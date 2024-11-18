f23e1ef2df278bfb2e630ac976f7e3d584fe09e6
PC Ubuntu <glyanenkoiv@gmail.com>
2024-11-18 12:24:03 +0700

    Initial commit

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -79,12 +79,14 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
-        'USER': 'postgres',
-        'PASSWORD': 'Qwerty12@',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': 'netology_stocks_products',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'qwerty12',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
